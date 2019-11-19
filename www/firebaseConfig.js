be549e8a9ed8a9b4cbb6ae3297a493307958f65d
nicolas102003 <nicolas2003keko@gmail.com>
2019-11-19 09:29:08 -0300

    firebase1

---
+++ www/firebaseConfig.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAoRZ3NmsSgBbC-VWJqoazGSIpqWu-_0Jg",
+    authDomain: "aula-de-cordova.firebaseapp.com",
+    databaseURL: "https://aula-de-cordova.firebaseio.com",
+    projectId: "aula-de-cordova",
+    storageBucket: "aula-de-cordova.appspot.com",
+    messagingSenderId: "725186317154",
+    appId: "1:725186317154:web:639aec5f37a0d2050c7a3b",
+    measurementId: "G-R4BHK9T635"
+  };
---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    onDeviceReady: function() {        
+        onLoadFunction();
+    }
+};
+app.initialize();
